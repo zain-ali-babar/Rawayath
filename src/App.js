3da08e655b054836b05fc251fc1daad29653734b
Zain Ali Babar <xalibabar@gmail.com>
2021-04-11 02:21:11 +0500

    Created Shopping Car and placed order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
-
-import { BrowserRouter,Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import "./styles.css";
 import Login from "./Screens/Login";
@@ -12,24 +10,22 @@ import HomePage from "./Screens/homepage.js";
 import AddProduct from "./Screens/addproduct";
 import Shoppingcart from "./Screens/shopping_cart";
 import ProductDescription from "./Screens/productdescription";
+import ApproveOrders from "./Screens/approve_prod";
 function App() {
   return (
     <>
-    <div className="App">
-      {/* <Route exact path="/" component={EditProfile} /> */}
-      {/* <Route exact path="/" component={AddProduct} /> */}
-    
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/shoppingcart" component={Shoppingcart} />
-      <Route exact path="/pd/:id" component={ProductDescription} />
-      <Route exact path="/admin" component={AdminDashboard} />
-      <Route exact path="/register" component={Register} />
+      <div className="App">
+        {/* <Route exact path="/" component={EditProfile} /> */}
+        <Route exact path="/addproducts" component={AddProduct} />
 
-
-    </div>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/shoppingcart" component={Shoppingcart} />
+        <Route exact path="/pd/:id" component={ProductDescription} />
+        <Route exact path="/admin" component={AdminDashboard} />
+        <Route exact path="/register" component={Register} />
+      </div>
     </>
-    
   );
 }
 
